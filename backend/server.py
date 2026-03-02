00a699493f95a532808b533bb06d9878a55d196e
Marvins Contracting <marvin@marvinscontracting.com>
2026-03-02 00:26:41 +0000

    auto-commit for 07344e9f-e0d2-4527-8ac6-ab3c9a651026

--- backend/server.py
+++ backend/server.py
@@ -11,7 +11,7 @@ import uuid
 from datetime import datetime, timezone
 
 # Import routes
-from routes import contact, gallery
+from routes import contact, gallery, admin
 
 
 ROOT_DIR = Path(__file__).parent
